dc76be017be38e9096cb3941201f8b8e0936de2c
Kim <jokercsi@gmail.com>
2022-12-08 13:24:56 +0900

    data mining

--- src/Morphological_Analysis.py
+++ src/Morphological_Analysis.py
@@ -3,6 +3,12 @@ import pathlib # pathlib 모듈의 기본 아이디어는 파일시스템 경로
 import pandas as pd
 import os
 
+# 형태소 분석 nltk
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.tag import pos_tag
+
 def parser_args():
 
     #현재 위치 확인
@@ -16,61 +22,60 @@ def parser_args():
 
     return parser.parse_args()
 
-# def stopword(data_path):
-#     with open(data_path, "r", encoding="utf-8") as f:
-#         stopwords = [w.strip() for w in f]
-#         stopwords = set(stopwords)
-#         f.close()
-#     return stopwords
+def stopword(data_path):
+    with open(data_path, "r", encoding="utf-8") as f:
+        stopwords = [w.strip() for w in f]
+        stopwords = set(stopwords)
+        f.close()
+    return stopwords
 
+# glob() 함수 : 파라미터에 명시된 저장 경로와 패턴에 해당하는 파일명을 리스트 형식으로 반환한다.
 def make_df(data_path):
-    # glob() = 파라미터에 명시된 저장 경로와 패턴에 해당하는 파일명을 리스트 형식으로 반환한다.
+
     news_csv = data_path.glob("*.csv")      #csv 파일들의 이름만 news_csv에 리스트에 저장된다.
     # pd.set_option("display.max_colwidth", 5400)
     # pd.set_option("display.max_rows", 5000)
-
-    #print(list(news_csv))  # csv 파일 확인
     news = pd.DataFrame()
     for f in news_csv:
         print(f)
-        tmp = pd.read_csv(f, encoding="utf8")
-
-        # tmp = tmp[(tmp["本文"] != "【現在著作権交渉中の為、本文は表示できません】") & ((tmp["面種別名"] == "経済面") | (tmp["面種別名"] == "経済")) & (tmp["発行区分"] == "朝刊")]
-        #news = pd.concat([news, tmp], axis=0)
-    # news["Date"] = news["Date"].astype(int)
-    # news = news.sort_values("Date")
-    # news = news.reset_index(drop=True)
-    return news_csv
+        tmp = pd.read_csv(f, encoding="utf8")   # 각각 파일의 읽은 내용
+        news = pd.concat([news, tmp], axis=0)   # 하나로 합치기
+    news['Date'] = pd.to_datetime(news['Date'], errors='raise') # 시간 형식 통일하기
+    news = news.sort_values("Date")
+    news = news.reset_index(drop=True)
+    return news
 
 
-# def Morphological(article, stopwords):
 
-#     mecab = MeCab.Tagger(r'-Owakati -d "./../data/mecab-ipadic-neologd"')
-#     text = article[8]
-#     text_out = ""
-#     text_result = ""
-#     node = mecab.parseToNode(text)
-#     while node:
-#         f = node.feature.split(",")
-#         for stopword in stopwords:
-#             if f[6] == stopword:
-#                 f[6] = "*"
-#         if (f[6] != "*") and (f[0] == "名詞"):
-#             text_out += f[6] + " "
-#         node = node.next
-#     text_out = text_out.replace("\u3000", "").replace("\n", "")
-#     text_out = re.sub(
-#         r"[0123456789０１２３４５６７８９！＠＃＄％＾＆\-|\\＊\“（）＿■×※⇒—●(：〜＋=)／*&^%$#@!~`){}…\[\]\"\'\”:;<>?＜＞？、。・,./『』【】「」→←○]+",
-#         "",
-#         text_out,
-#     )
-#     sentense = text_out.split(" ")
-#     sentense = list(filter(("").__ne__, sentense))
+# 
+def Morphological(article, stopwords):
 
-#     for word in sentense:
-#         text_result += word + " "
+    mecab = MeCab.Tagger(r'-Owakati -d "./../data/mecab-ipadic-neologd"')
+    text = article[8]
+    text_out = ""
+    text_result = ""
+    node = mecab.parseToNode(text)
+    while node:
+        f = node.feature.split(",")
+        for stopword in stopwords:
+            if f[6] == stopword:
+                f[6] = "*"
+        if (f[6] != "*") and (f[0] == "名詞"):
+            text_out += f[6] + " "
+        node = node.next
+    text_out = text_out.replace("\u3000", "").replace("\n", "")
+    text_out = re.sub(
+        r"[0123456789０１２３４５６７８９！＠＃＄％＾＆\-|\\＊\“（）＿■×※⇒—●(：〜＋=)／*&^%$#@!~`){}…\[\]\"\'\”:;<>?＜＞？、。・,./『』【】「」→←○]+",
+        "",
+        text_out,
+    )
+    sentense = text_out.split(" ")
+    sentense = list(filter(("").__ne__, sentense))
 
-#     return text_result, len(sentense)
+    for word in sentense:
+        text_result += word + " "
+
+    return text_result, len(sentense)
 
 
 def get_wordlist_in_text(args):
@@ -85,17 +90,20 @@ def get_wordlist_in_text(args):
     # print(path_output)
 
     # 함수 사용
-    news = make_df(path_data)
-    print(news)
-    # stopwords = stopword(path_stopwords)
+    news = make_df(path_data)   # 뉴스데이터 통합하고 가져오기
+    #news.to_csv('output.csv',index=False, encoding="utf-8")
+    stopwords = stopword(path_stopwords)    # stopword 가져오기
+    #print(news)
+    #print(stopwords)
 
-    # articles = []
-    # word_len = []
 
-    # for sentense in tqdm(news.values):
-    #     word_list = Morphological(sentense, stopwords)
-    #     word_len.append(word_list[1])
-    #     articles.append(word_list[0])
+    articles = []
+    word_len = []
+
+    for sentense in tqdm(news.values):
+        word_list = Morphological(sentense, stopwords)
+        word_len.append(word_list[1])
+        articles.append(word_list[0])
 
     # for i, (article, word) in enumerate(zip(articles, word_len)):
     #     news.loc[i, "本文"] = article
